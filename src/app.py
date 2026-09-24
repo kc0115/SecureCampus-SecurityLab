012a0efc874404dd75cf450e5b34a018756ec0ed
kc0115 <flanblue@gmail.com>
2026-09-24 07:30:49 -0600

    estructura correcta de codigo

--- src/app.py
+++ src/app.py
@@ -1,20 +1,12 @@
-import sqlite3
-
-
 def buscar_estudiante(nombre):
     conexion = sqlite3.connect("securecampus.db")
     cursor = conexion.cursor()
     consulta = (
         "SELECT id, nombre, correo "
         "FROM estudiantes "
-        "WHERE nombre = '" + nombre + "'"
+        "WHERE nombre = ?"
     )
-    cursor.execute(consulta)
+    cursor.execute(consulta, (nombre,))
     resultado = cursor.fetchall()
     conexion.close()
     return resultado
-
-
-nombre = input("Nombre del estudiante: ")
-estudiantes = buscar_estudiante(nombre)
-print(estudiantes)
